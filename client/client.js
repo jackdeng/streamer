34804047b0bbbfbfcef9adefca720d39a7fb85d0
Jack Deng <k.jackdeng@gmail.com>
2014-12-13 15:16:45 -0800

    save userid to document cookie.

--- client/client.js
+++ client/client.js
@@ -1,5 +1,11 @@
 Meteor.startup(function() {
 	Tracker.autorun(function() {
-		console.log("Starting Meteor... Starting Streamer...");
+		console.log("/*** Meteor startup ***/");
+
+		// save cookies	
+		if (Meteor.userId()) {
+			console.log("UserId found, saving to Cookie");	
+			Util.setCookie("meteor_userid", Meteor.userId(), 1);
+		}
 	});
 });
